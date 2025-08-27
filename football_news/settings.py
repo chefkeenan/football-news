6226d52cd266412763a15e1fe6da9fb58ec6d2cc
Keenan <aryasatyakeenan@gmail.com>
2025-08-27 16:40:03 +0700

    ngefix  url deployment pws di allowedhost

--- football_news/settings.py
+++ football_news/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-2&%$h-m)h)xn)#$52c%)72)=11@b7)#rh&_cwxxa!!4%wk@*85
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ahmad.keenan-footballnews.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ahmad-keenan-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
