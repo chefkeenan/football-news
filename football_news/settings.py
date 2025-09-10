dbbf45307c224eb044c581d48535cd0a1c96084a
Keenan <aryasatyakeenan@gmail.com>
2025-09-10 20:32:41 +0700

    csrf trusted origin fix

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ahmad-keenan-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://ahmad-keenan-footballnews.pbp.cs.ui.ac.id/"
+    "https://ahmad-keenan-footballnews.pbp.cs.ui.ac.id"
 ]
 
 
